224a6b42ae6a28c1a688474bc1abfff506c2fc43
chhak0503 <chhak0503@gmail.com>
2024-07-09 16:41:44 +0900

    sql practice 2

--- SQL 연습문제2.sql
+++ SQL 연습문제2.sql
@@ -75,22 +75,172 @@ INSERT INTO `bank_transaction` (`t_a_no`, `t_dist`, `t_amount`, `t_datetime`) VA
 #실습 2-9
 #실습 2-10
 #실습 2-11
+select * from `bank_customer` where `c_addr` like '경기도%';
+
 #실습 2-12
 #실습 2-13
 #실습 2-14
+select * from `bank_transaction` where `t_dist` =2 order by `t_amount` desc;
+
 #실습 2-15
+select 
+	SUM(`t_amount`) as `입금총합`,
+    AVG(`t_amount`) as `입금평균`
+from
+	`bank_transaction`
+where
+	`t_dist`=1;
+
+
+
 #실습 2-16
 #실습 2-17
+select * from `bank_account` 
+where `a_item_dist` = 'S1'
+order by `a_balance` desc;
+
 #실습 2-18
+select * from `bank_account` where `a_item_dist` = 'S1'
+order by `a_balance` desc limit 1;
+
+select * from `bank_account` 
+	where `a_balance` = (select MAX(`a_balance`) 
+							from `bank_account` 
+							where `a_item_dist`='S1');
+
 #실습 2-19
+select * from `bank_transaction` where `t_dist` = 1 or `t_dist` = 2
+order by `t_dist`, `t_amount` desc; 
+
+select * from `bank_transaction` where `t_dist` IN (1, 2)
+order by `t_dist`, `t_amount` desc; 
+
+
+
 #실습 2-20
+select 
+	count(case when `t_dist` = 1 then `t_dist` end) as `입금 건수`,
+    count(if(`t_dist`=2, 1, null)) as `출금 건수`,
+    count(if(`t_dist`=3, 1, null)) as `조회 건수`
+from `bank_transaction`;
+
+
+
 #실습 2-21
+select
+	`t_dist`,
+    CASE
+		WHEN(`t_dist`=1) THEN '입금'
+        WHEN(`t_dist`=2) THEN '출금'
+        WHEN(`t_dist`=3) THEN '조회'
+    END AS `type`,
+    `t_a_no`,
+    `t_amount`
+from
+	`bank_transaction`;
+
 #실습 2-22
+select 
+	`t_dist`, 
+	COUNT(`t_no`) 
+from 
+	`bank_transaction` 
+group by 
+	`t_dist`;
+
+
 #실습 2-23
+select `t_a_no`, `t_dist`, SUM(`t_amount`)
+from `bank_transaction`
+where `t_dist`=1
+group by `t_a_no`;
+
+
 #실습 2-24
+select `t_a_no`, `t_dist`, SUM(`t_amount`) as `합계`
+from `bank_transaction`
+where `t_dist`=1
+group by `t_a_no`
+having `합계` >= 100000
+order by `합계` desc;
+
 #실습 2-25
+select * from `bank_account` as a
+join `bank_customer` as b
+on a.a_c_no = b.c_no;
+
+select * from `bank_account` a, `bank_customer` b where a.a_c_no = b.c_no;
+
 #실습 2-26
+select 
+	`a_no` AS `계좌번호`, 
+	`a_item_name` AS `계좌이름`,
+	`c_no` AS `주민번호(사업자번호)`,
+	`c_name` AS `고객명`,
+	`a_balance` AS `현재잔액`
+from
+	`bank_account` as a
+join 
+	`bank_customer` as b
+on 
+	a.a_c_no = b.c_no;
+	
+
 #실습 2-27
+select * from `bank_transaction` as a
+join `bank_account` as b
+on a.t_a_no = b.a_no;
+
 #실습 2-28
+select 
+	`t_no` AS `거래번호`,
+	`t_a_no` AS `계좌번호`,
+	`a_c_no` AS `고객번호(주민번호)`,
+	`t_dist` AS `거래구분`,
+	`t_amount` AS `거래금액`,
+	`t_datetime` AS `거래일자`
+from
+	`bank_account` as a
+join 
+	`bank_transaction` as b
+on 
+	a.a_no = b.t_a_no;
+
+
 #실습 2-29
+select
+	`t_no`,
+	`a_no`,
+	`c_no`,
+	`t_dist`,
+	`a_item_name`,
+	`c_name`,
+	`t_amount`,
+	`t_datetime`
+from `bank_transaction` as a
+join `bank_account` as b on a.t_a_no = b.a_no
+join `bank_customer` as c on b.a_c_no = c.c_no
+where `t_dist` = 1
+order by `t_amount` desc;
+
 #실습 2-30
+SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SELECT 
+	`t_no`,
+	`a_no`,
+	`c_no`,
+	`t_dist` AS `구분`,
+	`a_item_name`,
+	`c_name`,
+	COUNT(`t_no`) AS `거래건수`
+FROM `bank_transaction` AS a
+JOIN `bank_account` AS b ON a.t_a_no = b.a_no
+JOIN `bank_customer` AS c ON b.a_c_no = c.c_no
+WHERE `t_dist` IN(1, 2) AND `c_dist` = 1
+GROUP BY `c_no`
+ORDER BY `구분`, `거래건수` DESC;
+
+
+
+
+
