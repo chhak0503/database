08e87ce4f0d3196daae865ff8835821ccf3a73b1
chhak0503 <chhak0503@gmail.com>
2024-07-12 16:29:05 +0900

    sql practice5

--- SQL 연습문제5.sql
+++ SQL 연습문제5.sql
@@ -61,45 +61,217 @@ INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (2,10,7000
 INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (3, 8,13000,'2014-07-10');
 
 #실습 5-4
+SELECT `custId`, `name`, `address` FROM `Customer`;
+
 #실습 5-5
+SELECT `bookName`, `price` FROM `Book`;
+
 #실습 5-6
+SELECT `price`, `bookName` FROM `Book`;
+
 #실습 5-7
+SELECT * FROM `Book`;
+
 #실습 5-8
+SELECT `publisher` FROM `Book`;
+
 #실습 5-9
+SELECT DISTINCT `publisher` FROM `Book`;
+SELECT `publisher` FROM `Book` GROUP BY `publisher`;
+
 #실습 5-10
+SELECT * FROM `Book` WHERE `price` >= 20000;
+
 #실습 5-11
+SELECT * FROM `Book` WHERE `price` < 20000;
+
 #실습 5-12
+SELECT * FROM `Book` WHERE `price` >= 10000 AND `price` <= 20000;
+
 #실습 5-13
+SELECT `bookId`, `bookName`,`price` 
+FROM `Book` 
+WHERE `price` >= 15000 AND `price` <= 30000;
+
 #실습 5-14
+SELECT * FROM `Book` WHERE `bookId`=2 OR `bookId`=3 OR `bookId`=5;
+SELECT * FROM `Book` WHERE `bookId` IN(2, 3, 5);
+
 #실습 5-15
+SELECT * FROM `Book` WHERE `bookId` % 2 = 0;
+SELECT * FROM `book` WHERE MOD(bookId, 2) =0;
+
 #실습 5-16
+SELECT * FROM `Customer` WHERE `name` LIKE '박%';
+
 #실습 5-17
+SELECT * FROM `Customer` WHERE `address` LIKE '%대한민국%';
+
 #실습 5-18
+SELECT * FROM `Customer` WHERE `phone` IS NOT NULL;
+
 #실습 5-19
+SELECT * FROM `Book` WHERE `publisher`='굿스포츠' OR `publisher`='대한미디어';
+SELECT * FROM `Book` WHERE `publisher` IN('굿스포츠', '대한미디어');
+
 #실습 5-20
+SELECT `publisher` FROM `Book` WHERE `bookName`='축구의 역사';
+
 #실습 5-21
+SELECT `publisher` FROM `Book` WHERE `bookName` LIKE '%축구%';
+
 #실습 5-22
+SELECT * FROM `Book` WHERE `bookName` LIKE '_구%';
+
 #실습 5-23
+SELECT * FROM `Book` WHERE `bookName` LIKE '%축구%' AND `price` >= 20000;
+
 #실습 5-24
+SELECT 
+    *
+FROM
+    `Book`
+ORDER BY `bookName`;
+
 #실습 5-25
+SELECT * FROM `Book` order BY `price`, `bookName`;
+
 #실습 5-26
+SELECT * FROM `Book` order BY `price` DESC, `bookName` ASC;
+
 #실습 5-27
+SELECT * FROM `Book` order BY `price` DESC LIMIT 3;
+
 #실습 5-28
+SELECT * FROM `Book` order BY `price` ASC LIMIT 3;
+
 #실습 5-29
+SELECT SUM(`salePrice`) AS `총판매액` FROM `Order`;
+
 #실습 5-30
+SELECT 
+	SUM(`salePrice`) AS `총판매액`,
+	AVG(`salePrice`) AS `평균값`,
+	MIN(`salePrice`) AS `최저가`,
+	MAX(`salePrice`) AS `최고가`
+FROM `Order`;
+
+
 #실습 5-31
+SELECT COUNT(*) AS `판매건수` FROM `Order`;
+
 #실습 5-32
+SELECT 
+	`bookId`,
+	REPLACE(`bookName`, '야구', '농구') AS `bookName`,
+	`publisher`,
+	`price`
+FROM `Book`;
+
 #실습 5-33
+SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+select 
+	`custId`,
+    count(*) as `수량`
+from `order` 
+where `salePrice` >= 8000
+group by `custId`
+having `수량` >= 2;
+	
 #실습 5-34
+SELECT * FROM `Customer` AS a 
+JOIN `Order` AS b  ON a.custId = b.custId;
+
+SELECT * FROM `Customer` JOIN `Order` USING(`custId`);
+
+SELECT * FROM `Customer` a, `Order` b 
+	WHERE a.custId = b.custId;
+
+
 #실습 5-35
+SELECT * FROM `Customer` AS a 
+JOIN `Order` AS b  ON a.custId = b.custId
+order BY a.`custId`;
+
 #실습 5-36
+SELECT
+	`name`,
+	`salePrice`
+FROM `Customer` AS a 
+JOIN `Order` AS b  ON a.custId = b.custId;
+
 #실습 5-37
+SELECT
+	`name`,
+	SUM(`salePrice`)
+FROM `Customer` AS a 
+JOIN `Order` AS b  ON a.custId = b.custId
+group by `name`
+order by `name`;
+
+
 #실습 5-38
+SELECT 
+	`name`,
+	`bookName`
+FROM `Customer` AS a 
+JOIN `Order` AS b ON a.custId = b.custId
+JOIN `Book` AS c ON b.bookId = c.bookId;
+
 #실습 5-39
+SELECT 
+	`name`,
+	`bookName`
+FROM `Customer` AS a 
+JOIN `Order` AS b ON a.custId = b.custId
+JOIN `Book` AS c ON b.bookId = c.bookId
+where `salePrice`=20000;
+
 #실습 5-40
+SELECT 
+	`name`,
+    `salePrice`
+FROM `Customer` AS a 
+left JOIN `Order` AS b ON a.custId = b.custId;
+
 #실습 5-41
+SELECT 
+	SUM(`salePrice`) AS `총매출`
+FROM `Customer` AS a
+JOIN `Order` AS b ON a.custId = b.custId
+WHERE `name`='김연아';
+
+SELECT 
+	SUM(`salePrice`) AS `총매출`
+FROM `Order`
+WHERE `custId`=(SELECT `custId` FROM `Customer` WHERE `name`='김연아');
+
+
 #실습 5-42
+SELECT `bookName` FROM `Book` ORDER BY `price` DESC LIMIT 1;
+SELECT `bookName` FROM `Book` WHERE `price`=(SELECT MAX(`price`) FROM `Book`);
+
 #실습 5-43
+SELECT `name` FROM `Customer` AS a 
+LEFT JOIN `Order` AS b ON a.custId = b.custId
+WHERE `orderId` IS NULL;
+
 #실습 5-44
+INSERT INTO `Book` SET
+					`bookId` = 11,
+					`bookName` = '스포츠의학',
+					`publisher` = '한솔의학서적';
+
 #실습 5-45
+UPDATE `Customer` SET `address`='대한민국 부산' WHERE `custId`=5;
+
 #실습 5-46
+DELETE FROM `Customer` WHERE `custId`=5;
+
+
+
+
+
+
+
+
